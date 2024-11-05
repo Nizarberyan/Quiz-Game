fac1b1a4ddee8e08fa1e3afba8140cd72828a087
Nizar Beriane <nizarberiane43@gmail.com>
2024-11-05 16:04:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-red-500 text-white p-4 flex justify-center items-center flex-col">
+      <h1>Hello, Tailwind!</h1>
+      <h1>Hello, Tailwind!</h1>
+      <h1>Hello, Tailwind!</h1>
+      <h1>Hello, Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
