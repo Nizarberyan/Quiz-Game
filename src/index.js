e12f331a8211ec272a30fbf37116025403eaae0f
Nizar Beriane <nizarberiane43@gmail.com>
2024-11-08 15:11:50 +0100

    edit

--- src/index.js
+++ src/index.js
@@ -1,92 +1,5 @@
-let quizzes = [
-  {
-    id: 1,
-    title: "Web Development Fundamentals",
-    description: "Test your knowledge of HTML, CSS, and JavaScript basics.",
-    difficulty: "Easy",
-    numQuestions: 10,
-    duration: 15, // minutes
-  },
-  {
-    id: 2,
-    title: "React Mastery",
-    description:
-      "Assess your React skills, from components to state management.",
-    difficulty: "Medium",
-    numQuestions: 20,
-    duration: 30,
-  },
-  {
-    id: 3,
-    title: "Node.js and Express.js",
-    description:
-      "Gauge your understanding of server-side JavaScript and web frameworks.",
-    difficulty: "Medium",
-    numQuestions: 15,
-    duration: 25,
-  },
-  {
-    id: 4,
-    title: "Database Design and SQL",
-    description: "Test your knowledge of database concepts and SQL queries.",
-    difficulty: "Hard",
-    numQuestions: 25,
-    duration: 45,
-  },
-  {
-    id: 5,
-    title: "Python Programming Basics",
-    description: "Assess your Python programming fundamentals.",
-    difficulty: "Easy",
-    numQuestions: 12,
-    duration: 20,
-  },
-  {
-    id: 6,
-    title: "Data Science with Python",
-    description:
-      "Test your skills in data analysis, machine learning, and data visualization.",
-    difficulty: "Hard",
-    numQuestions: 30,
-    duration: 60,
-  },
-  {
-    id: 7,
-    title: "JavaScript Algorithms and Data Structures",
-    description:
-      "Gauge your problem-solving and algorithmic thinking abilities.",
-    difficulty: "Medium",
-    numQuestions: 18,
-    duration: 35,
-  },
-  {
-    id: 8,
-    title: "Cybersecurity Fundamentals",
-    description:
-      "Test your knowledge of cybersecurity concepts and best practices.",
-    difficulty: "Medium",
-    numQuestions: 15,
-    duration: 25,
-  },
-  {
-    id: 9,
-    title: "Networking Basics",
-    description: "Assess your understanding of network protocols and concepts.",
-    difficulty: "Easy",
-    numQuestions: 10,
-    duration: 15,
-  },
-  {
-    id: 10,
-    title: "Cloud Computing Concepts",
-    description:
-      "Test your knowledge of cloud computing platforms and services.",
-    difficulty: "Medium",
-    numQuestions: 18,
-    duration: 30,
-  },
-];
-
+quizzes = JSON.parse(localStorage.getItem("quizzes")) || [];
+console.log(quizzes);
 document.querySelector(".theme-btn").addEventListener("click", () => {
   const isDark = localStorage.getItem("isDark") === "true";
   localStorage.setItem("isDark", !isDark);
@@ -111,35 +24,44 @@ document.querySelector(".close").addEventListener("click", () => {
 document.addEventListener("DOMContentLoaded", () => {
   const quizzesContainer = document.querySelector(".quizzes");
 
-  function createQuiz(title, description, difficulty, numQuestions, duration) {
+  function createQuiz(
+    id,
+    title,
+    description,
+    difficulty,
+    numQuestions,
+    duration,
+    cover
+  ) {
     const quizElement = document.createElement("div");
     quizElement.classList.add(
       "quiz",
       "bg-white",
       "dark:bg-dark-primary-dark",
-      "h-[50vh]",
+
       "shadow-[0_4px_12px_-5px_rgba(0,0,0,0.4)]",
       "w-full",
       "max-w-sm",
       "rounded-lg",
       "overflow-hidden",
-      "mx-auto",
       "font-[sans-serif]",
       "mt-4",
       "flex",
       "flex-col"
     );
 
-    const htmlContent = `
-  <img src="images/thumbnail.png" alt="thumbnail" class="w-full flex-shrink-0" />
-  <div class="p-3 flex-1 flex flex-col">
-    <h3 class="text-lg font-medium dark:text-primary-light">${title}</h3>
-    <p class="dark:text-primary-light">${description}</p>
-    <p class="text-sm font-light text-gray-500 dark:text-primary-light">Difficulty: ${difficulty}</p>
-    <p class="text-sm font-light text-gray-500 dark:text-primary-light">${numQuestions} questions</p>
-    <p class="text-sm font-light text-gray-500 dark:text-primary-light">Duration: ${duration} minutes</p>
-  </div>
-`;
+    const htmlContent = `<p class="hidden">${id}</p>
+    <img src="${cover}" alt="thumbnail" class="w-full flex-shrink-0" />
+    <div class=" p-2 flex-1 flex flex-col gap-2">
+        <h3 class="text-lg font-medium dark:text-primary-light">${title}</h3>
+        <p class="dark:text-primary-light">${description}</p>
+        <p class="text-sm font-light text-gray-500 dark:text-primary-light">Difficulty: ${difficulty}</p>
+        <p class="text-sm font-light text-gray-500 dark:text-primary-light">${numQuestions} questions</p>
+        <p class="text-sm font-light text-gray-500 dark:text-primary-light">Duration: ${duration} minutes</p>
+         </div>
+         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-b">
+            Start Quiz
+        </button>`;
     quizElement.innerHTML = htmlContent;
 
     return quizElement;
@@ -148,11 +70,13 @@ document.addEventListener("DOMContentLoaded", () => {
   quizzes.forEach(function (quiz) {
     quizzesContainer.appendChild(
       createQuiz(
+        quiz.id,
         quiz.title,
         quiz.description,
         quiz.difficulty,
         quiz.numQuestions,
-        quiz.duration
+        quiz.duration,
+        quiz.cover
       )
     );
   });
